d27f65f46974dfdb9b60f5190de0467a95e7911f
pedrorfs <pedrorfs11@gmail.com>
2025-03-31 00:40:51 -0300

    Update collector

--- collector.py
+++ collector.py
@@ -53,43 +53,54 @@ def capture_response(response):
         # Print and store the response URL and body (if it's JSON)
         try:
             response_data = response.json()
+            print("Videos from profile: ", response_data["itemList"][0]["authorStats"]["videoCount"])
             response_data = normalize_response(response_data)
             responses.update(response_data)  # Store the response data
-            # print(f"Captured Response: {response.url}")
+            print(f"Responses size: {len(responses)}")
         except Exception as e:
             print(f"Error parsing response: {e}")
+            exit()
 
 def scroll_page(page):
     # Scroll down by a certain amount
     page.evaluate("window.scrollBy(0, window.innerHeight)")
     page.wait_for_timeout(2000)  # Wait for 1 second after scrolling
 
-with sync_playwright() as pw:
-    browser = pw.chromium.launch(headless=False)
-    page = browser.new_page()
+def scrape(username, max_videos=1000):
+    with sync_playwright() as pw:
+        browser = pw.chromium.launch(headless=False)
+        page = browser.new_page()
+        try:
+            # Listen for responses
+            page.on("response", capture_response)
 
-    # Listen for responses
-    page.on("response", capture_response)
+            # Navigate to the page
+            page.goto(f"https://www.tiktok.com/@{username}")
 
-    # Navigate to the page
-    page.goto("https://www.tiktok.com/@enaldinho")
+            # Wait for the page to load initially
+            page.wait_for_load_state("networkidle")
 
-    # Wait for the page to load initially
-    page.wait_for_load_state("networkidle")
+            # Scroll and capture responses multiple times
+            while(len(responses) <= max_videos):
+                scroll_page(page)
+                if (len(responses) >= max_videos):
+                    break
 
-    # Scroll and capture responses multiple times
-    for _ in range(10):  # Scroll 5 times as an example
-        scroll_page(page)
-    
-    # Wait for responses to be captured
-    page.wait_for_timeout(3000)  # Wait 2 seconds to capture all responses
+        except Exception as e:  # Catch any other exception
+            print(f"Some error occurred: {e}")
+        finally:
+            # save the captured responses to a file
+            with open("responses.json", "w", encoding="utf-8") as f:
+                json.dump(responses, f, ensure_ascii=False, indent=4)
 
-    # Optionally, save the captured responses to a file
-    with open("responses.json", "w", encoding="utf-8") as f:
-        json.dump(responses, f, ensure_ascii=False, indent=4)
-    
-    # Print the number of responses captured
-    print(f"Captured {len(responses)} responses")
+            # Wait for responses to be captured
+            page.wait_for_timeout(2000)  # Wait 2 seconds to capture all responses
 
-    # Close the browser
-    browser.close()
+            # Print the number of responses captured
+            print(f"Captured {len(responses)} responses")
+
+            # Close the browser
+            browser.close()
+
+if __name__ == "__main__":
+    scrape("gatogalactico", 1000)
